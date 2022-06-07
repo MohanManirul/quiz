b3ef0a85b2383c56335bbf16b29696c39367cfb1
MohanManirul <fiforeg@gmail.com>
2022-06-07 15:17:54 +0600

    district seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserProfileSeeder::class,
             SuperAdminSeeder::class,
+            DistrictSeeder::class,
         ]);
     }
 }
